978f0d999fa2752fe2b6254683e420125368464c
Courtney Buratto <cmburatto@gmail.com>
2020-03-23 09:43:56 -0700

    Set up for redux

--- notes/src/index.js
+++ notes/src/index.js
@@ -5,9 +5,15 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { createStore } from 'redux'
+import {Provider } from 'react-redux'
+import reducer from './reducers'
+
+const store = createStore(reducer)
+
 ReactDOM.render(
   <Router>
-    <App />
+    <Provider store ={store}><App /></Provider>
   </Router>,
   document.getElementById("root")
 );
