16c65d02970486a0f1f9679651420190ffa1a860
Courtney Buratto <cmburatto@gmail.com>
2020-03-24 08:41:33 -0700

    Begin to migrate ListView to use hooks

--- notes/src/components/ListView.js
+++ notes/src/components/ListView.js
@@ -1,56 +1,52 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from 'react'
 
 import {
-  ListViewWrapper,
-  NotesViewWrapper,
-  NotesListTitle,
-  NoteCard,
-  NoteCardLink,
-  NoteCardList,
-  NoteCardTitle,
-  NoteCardText
-} from "./Styled";
-import axios from "axios";
+	ListViewWrapper,
+	NotesViewWrapper,
+	NotesListTitle,
+	NoteCard,
+	NoteCardLink,
+	NoteCardList,
+	NoteCardTitle,
+	NoteCardText
+} from './Styled'
+import axios from 'axios'
 
-class ListView extends Component {
-  constructor() {
-    super();
-    this.state = {
-      notes: []
-    };
-  }
+const ListView = props => {
+	const [viewNote, setViewNote] = useState({
+		...props,
+		notes: []
+	})
 
-  componentDidMount() {
-    axios
-      .get("https://fe-notes.herokuapp.com/note/get/all")
-      .then(res => {
-        this.setState({ notes: res.data });
-      })
-      .catch(err => console.log(err, "failed to get api"));
-  }
+	useEffect(() => {
+		axios
+			.get('https://fe-notes.herokuapp.com/note/get/all')
+			.then(res => {
+				setViewNote({ notes: res.data })
+			})
+			.catch(err => console.log(err, 'failed to get api'))
+	})
 
-  render() {
-    return (
-      <ListViewWrapper>
-        <NotesViewWrapper>
-          <NotesListTitle>Your Notes:</NotesListTitle>
+	return (
+		<ListViewWrapper>
+			<NotesViewWrapper>
+				<NotesListTitle>Your Notes:</NotesListTitle>
 
-          <NoteCardList>
-            {this.state.notes.map(note => {
-              return (
-                <NoteCardLink to={`/note/${note._id}`} key={note._id}>
-                  <NoteCard>
-                    <NoteCardTitle>{note.title}</NoteCardTitle>
-                    <NoteCardText>{note.textBody}</NoteCardText>
-                  </NoteCard>
-                </NoteCardLink>
-              );
-            })}
-          </NoteCardList>
-        </NotesViewWrapper>
-      </ListViewWrapper>
-    );
-  }
+				<NoteCardList>
+					{[...viewNote.notes].map(note => {
+						return (
+							<NoteCardLink to={`/note/${note._id}`} key={note._id}>
+								<NoteCard>
+									<NoteCardTitle>{note.title}</NoteCardTitle>
+									<NoteCardText>{note.textBody}</NoteCardText>
+								</NoteCard>
+							</NoteCardLink>
+						)
+					})}
+				</NoteCardList>
+			</NotesViewWrapper>
+		</ListViewWrapper>
+	)
 }
 
-export default ListView;
+export default ListView
